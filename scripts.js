1169c2d1defe78532223c2fbf65750e21c45c3e4
caio lucena <caio.lcolaco@gmail.com>
2021-10-20 19:23:42 -0300

    added videos on modal

--- scripts.js
+++ scripts.js
@@ -4,11 +4,15 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
+
     })
 }
 
 
 document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ``
 })
