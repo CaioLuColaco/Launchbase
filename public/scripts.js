00d67e5734e126ea1b3fd8edd9b086944e45a33b
caio lucena <caio.lcolaco@gmail.com>
2021-10-29 12:53:29 -0300

    redirecting video page url

--- public/scripts.js
+++ public/scripts.js
@@ -5,6 +5,7 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
     card.addEventListener("click", function(){
         const videoId = card.getAttribute("id")
+        window.location.href = `/video?id=${videoId}`
     })
 }
 
